557c6d04e6165238357b849ac6ede0169260b9e9
Adam Wells <alwells408@gmail.com>
2020-06-17 14:32:19 -0400

    Reinit and install npm packages

---
+++ pages/index.js
@@ -0,0 +1,4 @@
+// Import Packages
+import React, { useState } from 'react'
+
+// baseEndpoint = https://www.instagram.com/web/search/topsearch/?context=blended&rank_token=0.5563254992883178&include_reel=true&query=%23
